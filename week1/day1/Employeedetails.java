68c8bb99c5c4f36459ea932de0998a16e3c2989e
Kaviya0805 <kaviyasundaramurthy@gmail.com>
2025-12-03 19:37:59 +0400

    Week1day1

---
+++ week1/day1/Employeedetails.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Employeedetails {
+	public static void main(String[] args) 
+	{
+		String name = "John Doe";
+		int age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		String mobilenumber = "9876543210";
+		float rating = 4.5f;
+		int pincode = 56001;
+		int luckynumber = 7;
+		System.out.println("Name of the employee " + name);
+		System.out.println("Age " + age);
+		System.out.println("Salary of the Employee "+ salary);
+		System.out.println("Gender " + gender);
+		System.out.println("Terms - " + acceptTerms);
+		System.out.println("Mobile Number - " + mobilenumber);
+		System.out.println("Pincode - " + pincode);
+		System.out.println("Lucky number - " + luckynumber);
+		
+	}
+
+}
